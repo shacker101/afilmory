80aa9c07179a61171c6e9a376fbb801cd921c006
shacker101 <qq1583362322@gmail.com>
2026-07-28 02:22:43 +0800

    fix OG preview

--- be/apps/core/src/modules/content/og/og.controller.ts
+++ be/apps/core/src/modules/content/og/og.controller.ts
@@ -1,9 +1,13 @@
 import { ContextParam, Controller, Get, Param } from '@tsuki-hono/common'
+import { AllowPlaceholderTenant } from '@core/decorators/allow-placeholder.decorator'
+import { SkipTenantGuard } from '@core/decorators/skip-tenant.decorator'
 import type { Context } from 'hono'
 
 import { OgService } from './og.service'
 
 @Controller({ prefix: '/og', bypassGlobalPrefix: true })
+@SkipTenantGuard()
+@AllowPlaceholderTenant()
 export class OgController {
   constructor(private readonly ogService: OgService) {}
 
